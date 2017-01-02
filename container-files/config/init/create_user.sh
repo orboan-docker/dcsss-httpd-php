69195a7b097c4ba074f116fe3869d153db1d6ee2
Oriol Boix Anfosso <dev@orboan.com>
2017-01-02 12:39:28 -0500

    Added option to change default ssh credentials

--- container-files/config/init/create_user.sh
+++ container-files/config/init/create_user.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-user=$USER
+#www user is always created. It's the user used by httpd!
+user=www
 pwd=$PASSWORD
 # Creating the www user only if it does not exist
 ret=false
@@ -19,4 +20,24 @@ echo "user created"
 fi
 mkdir -p /data/$user/html
 chown -R $user:$user /data/$user
+
+#Creating another user if $USER is not www
+user=$USER
+ret=false
+getent passwd $user >/dev/null 2>&1 && ret=true
+
+if $ret; then
+echo "user already exists";
+else
+
+useradd $user -d /home/$user
+# Setting password for the !www user
+echo "${user}:${pwd}" | chpasswd
+# Add !'www' user to sudoers
+echo "${user}  ALL=(ALL)  NOPASSWD: ALL" > /etc/sudoers.d/$user
+echo "user created"
+fi
+mkdir -p /home/$user
+cp /etc/skel/.b* /home/$user
+chown -R $user:$user /home/$user
 
